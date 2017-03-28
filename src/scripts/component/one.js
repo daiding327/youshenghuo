0c1dc4d2ffde3113d6e0ad645111fa6d2359a982
king <491958249@qq.com>
2017-03-28 21:38:31 +0800

    qian-luyou

---
+++ src/scripts/component/one.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class One extends React.Component {
+  constructor (props) {
+    super(props)
+    
+  }
+  
+  render() {
+	return (
+		<div className="m-my">
+			<ul>
+				<li>烹饪厨具</li>
+				<li>餐具</li>
+				<li>茶具餐饮</li>
+				<li>厨房配件</li>
+			</ul>
+		</div>
+	)	
+  }
+}
+
+export default One;
